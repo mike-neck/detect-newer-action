6e6b3fd39360028cf703565efacc3ee1f68fb834
mike_neck <jkrt3333@gmail.com>
2020-07-12 14:17:33 +0900

    rename excludes -> exclude-actions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as core from "@actions/core";
 
 try {
-    const excludes = core.getInput("excludes");
+    const excludes = core.getInput("exclude-actions");
     const outputFile = core.getInput("output-file");
     console.info(`excludes: ${excludes}, outputFile: ${outputFile}`)
 } catch (e) {
