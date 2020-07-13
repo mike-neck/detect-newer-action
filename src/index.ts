53ced6678937a0caa8735f93cacfcb00d8e2e1ce
mike_neck <jkrt3333@gmail.com>
2020-07-13 23:58:36 +0900

    refactor names.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,52 @@
 import * as core from "@actions/core";
-import {showPath} from "./io";
+import {listWorkflowFiles, readWorkflowFile} from "./io";
+import {getInputs, Inputs} from "./inputs";
+import {Either, right} from "./either";
+import {readWorkflow} from "./workflow";
 
 try {
     const excludes = core.getInput("exclude-actions");
     const outputFile = core.getInput("output-file");
     console.info(`excludes: ${excludes}, outputFile: ${outputFile}`)
 
-    showPath().then(files => {
+    listWorkflowFiles().then(files => {
         files.forEach(file => {
             console.info(`in .github/workflows: ${file}`)
         });
     })
 } catch (e) {
     core.setFailed(`error: ${e}`)
+}
+
+async function run(): Promise<void> {
+    const inputs = getInputs();
+    console.info(`input: ${inputs}`)
+    const workflowFiles = await listWorkflowFiles();
+    new Promise( async (success, failure) => {
+        for (let workflowFile of workflowFiles) {
+            if (!inputs.requireInspection(workflowFile)) {
+                continue;
+            }
+            const contents = await readWorkflowFile(workflowFile);
+            if (!contents.isRight()) {
+                failure(contents.fromLeft("fail"))
+                return;
+            }
+
+        }
+    })
+}
+
+async function ioOperation(inputs: Inputs): Promise<Either<string[], InspectionResult[]>> {
+    const workflowFiles = await listWorkflowFiles();
+    const result = new Array<InspectionResult>();
+    const failures = new Array<string>();
+    for (let workflowFile of workflowFiles) {
+        if (!inputs.requireInspection(workflowFile)) {
+            continue;
+        }
+        const contents = await readWorkflowFile(workflowFile)
+        const workflow = contents.flatMap(data => readWorkflow(data));
+
+    }
 }
--- src/io.ts
+++ src/io.ts
@@ -1,9 +1,26 @@
 import * as fs from "fs";
-import { Either, left, right } from "./either";
-import * as github from "@actions/github";
-import * as core from "@actions/core";
-import * as iou from "@actions/io/lib/io-util";
+import {Either, left, right} from "./either";
+import * as ioutil from "@actions/io/lib/io-util";
+import {WorkflowFile} from "./types";
 
-export async function showPath(): Promise<string[]> {
-    return iou.readdir(".github/workflows")
+export async function listWorkflowFiles(): Promise<WorkflowFile[]> {
+    const files = await ioutil.readdir(".github/workflows");
+    return  files.map(name => {
+        return { name: name, asPath: `.github/workflows/${name}` };
+    });
+}
+
+export async function readWorkflowFile(file: WorkflowFile): Promise<Either<string, string>> {
+    return new Promise<Either<string, string>>((success, _) => {
+        fs.readFile(
+            file.asPath,
+            {encoding: "utf-8"},
+            ((err, data) => {
+                if (err != null) {
+                    success(left(err.message));
+                } else {
+                    success(right(data));
+                }
+            }));
+    });
 }
